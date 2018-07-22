8d77b139e2938749f391ca97a666ddc0eda605ad
pinegit123 <liucs518@126.com>
2018-07-22 23:08:48 +0800

    v0.0.1 初次提交

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-alert('hello fast-cache');
+class FastCache{
+	constructor(){
+		this.list={}
+	}
+	set(key,value){
+		this.list[key]=value
+	}
+	get(key){
+		return this.list[key]
+	}
+}
+window.FastCache=FastCache
